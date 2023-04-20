878808cdf959e83d19c30e4031b574df83c7280c
김경민 <lifeandmyth@gmail.com>
2023-04-20 21:56:30 +0900

    임시 수정

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 # from .models import EggsD
-from .models import EggsD
+from food_costs.models import Eggs
 
 # Create your views here.
 
 def dashboard(request):
-  data = EggsD.objects.all()
-  name = EggsD.name
+  data = Eggs.objects.all()
+  name = Eggs.name
   context = {
     'dataset' : data,
     'name' : name
--- food_costs/models.py
+++ food_costs/models.py
@@ -7,17 +7,18 @@ class Eggs(models.Model):
     #날짜는 non-nullable field 값이기 때문에, default설정이 필수다.
     date = models.DateTimeField(default=timezone.now())
     unit_price = models.IntegerField()
+    name = "계란"
     
 
 class Flour(models.Model): 
     date = models.DateTimeField(default=timezone.now())
     unit_price = models.IntegerField()
-
+    name = "밀가루"
     
 class Sugar(models.Model): 
     date = models.DateTimeField(default=timezone.now())
     unit_price = models.IntegerField()
-
+    name = "설탕"
 
 #식량작물
 # class Rice(models.Model): 
